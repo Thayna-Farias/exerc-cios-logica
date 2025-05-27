95be00d8b29b70e784541180c7fd470b578dec92
Thayna-Farias <thaynafarias.200@gmail.com>
2025-05-27 17:08:24 -0300

    primeiro exercício idades.js

---
+++ idades.js
@@ -0,0 +1,11 @@
+// ler a idade dp usuário
+// se a idade for 18 ou mais, dizer que é maior
+// senão diz que é de menor 
+
+let idade = prompt("Qual sua idade?")
+if (idade >=18){
+    alert("Você é maior")
+}else{
+    alert ("Você é menor de idade.")
+
+}
